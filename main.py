7786ba77e1fe564d9e52f3762137d25e337c91fc
jayvakil-bc <jayvakil2@gmail.com>
2025-06-21 19:16:15 -0400

    Fix

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, APIRouter
 from services.youtube_scraper.routes import router as youtube_router
+from services.video_analyzer.routes import router as video_analyzer_router
 
 app = FastAPI(title="HackAI - Creator Analytics Backend")
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(youtube_router)
+api_router.include_router(video_analyzer_router)
 
 app.include_router(api_router)
 
